d168a86420d2560d50497115f5b00621f4e9aa31
Ajay Varma <ajayvarma2003@gmail.com>
2025-10-22 11:43:51 +0530

    Add files via upload

---
+++ C_Programming/40CallocDemo.c
@@ -0,0 +1,30 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main()
+{
+    int iLength = 0;
+    int *Arr = NULL;
+
+    printf("Enter the number of elements : \n");
+    scanf("%d", &iLength);
+
+    // Step 1 :- Allocate the memory
+    Arr = (int *) calloc (iLength, sizeof(int));
+
+    if(Arr == NULL)
+    {
+        printf("Unable to allocate memory \n");
+    }
+    else
+    {
+        printf("Memory gets successfully allocated \n");
+    }
+
+    // Step 2 :- Use the memory
+
+    // Step 3 :- Free the memory
+    free(Arr);
+
+    return 0;
+}
